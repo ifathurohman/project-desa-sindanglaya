ff466448d0950d1a9ca612d8b7c71336783638e7
ifathurohman <faturahman.ilham@gmail.com>
2025-05-16 01:36:58 +0700

    Add QueryClientProvider to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { AnimatePresence } from 'framer-motion';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 // Layout components
 import Layout from './components/layout/Layout';
@@ -15,24 +16,29 @@ import NewsPage from './pages/NewsPage';
 import NewsDetailPage from './pages/NewsDetailPage';
 import NotFoundPage from './pages/NotFoundPage';
 
+// Create a client
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <Router>
-      <AnimatePresence mode="wait">
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<HomePage />} />
-            <Route path="tourism" element={<TourismPage />} />
-            <Route path="products" element={<ProductsPage />} />
-            <Route path="budget" element={<BudgetPage />} />
-            <Route path="gallery" element={<GalleryPage />} />
-            <Route path="news" element={<NewsPage />} />
-            <Route path="news/:id" element={<NewsDetailPage />} />
-            <Route path="*" element={<NotFoundPage />} />
-          </Route>
-        </Routes>
-      </AnimatePresence>
-    </Router>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <AnimatePresence mode="wait">
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route index element={<HomePage />} />
+              <Route path="tourism" element={<TourismPage />} />
+              <Route path="products" element={<ProductsPage />} />
+              <Route path="budget" element={<BudgetPage />} />
+              <Route path="gallery" element={<GalleryPage />} />
+              <Route path="news" element={<NewsPage />} />
+              <Route path="news/:id" element={<NewsDetailPage />} />
+              <Route path="*" element={<NotFoundPage />} />
+            </Route>
+          </Routes>
+        </AnimatePresence>
+      </Router>
+    </QueryClientProvider>
   );
 }
 
