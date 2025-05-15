5ea153e99585e213c271e6d020b8b2fbe2f34dcb
ifathurohman <faturahman.ilham@gmail.com>
2025-05-16 02:10:36 +0700

    Fix Google Sheets data fetching

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { AnimatePresence } from 'framer-motion';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 // Layout components
 import Layout from './components/layout/Layout';
@@ -16,29 +15,24 @@ import NewsPage from './pages/NewsPage';
 import NewsDetailPage from './pages/NewsDetailPage';
 import NotFoundPage from './pages/NotFoundPage';
 
-// Create a client
-const queryClient = new QueryClient();
-
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <Router>
-        <AnimatePresence mode="wait">
-          <Routes>
-            <Route path="/" element={<Layout />}>
-              <Route index element={<HomePage />} />
-              <Route path="tourism" element={<TourismPage />} />
-              <Route path="products" element={<ProductsPage />} />
-              <Route path="budget" element={<BudgetPage />} />
-              <Route path="gallery" element={<GalleryPage />} />
-              <Route path="news" element={<NewsPage />} />
-              <Route path="news/:id" element={<NewsDetailPage />} />
-              <Route path="*" element={<NotFoundPage />} />
-            </Route>
-          </Routes>
-        </AnimatePresence>
-      </Router>
-    </QueryClientProvider>
+    <Router>
+      <AnimatePresence mode="wait">
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<HomePage />} />
+            <Route path="tourism" element={<TourismPage />} />
+            <Route path="products" element={<ProductsPage />} />
+            <Route path="budget" element={<BudgetPage />} />
+            <Route path="gallery" element={<GalleryPage />} />
+            <Route path="news" element={<NewsPage />} />
+            <Route path="news/:id" element={<NewsDetailPage />} />
+            <Route path="*" element={<NotFoundPage />} />
+          </Route>
+        </Routes>
+      </AnimatePresence>
+    </Router>
   );
 }
 
