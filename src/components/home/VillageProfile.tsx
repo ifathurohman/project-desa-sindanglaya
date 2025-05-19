e40b979c12ed5c80c5f9d7b030732828102a7bc8
ifathurohman <faturahman.ilham@gmail.com>
2025-05-19 14:12:56 +0700

    Create Village Profile Component

--- src/components/home/VillageProfile.tsx
+++ src/components/home/VillageProfile.tsx
@@ -115,7 +115,7 @@ const VillageProfile: React.FC = () => {
                         </div>
 
                         <div className="aspect-square rounded-lg overflow-hidden bg-gray-100">
-                            <iframe                             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31678.8802059115!2d107.19736885912606!3d-7.025734550454537!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f12!3m3!1m2!1s0x2e685eb54a60622d%3A0xbaa410fbc2298484!2sSindangjaya%2C%20Kec.%20Gununghalu%2C%20Kabupaten%20Bandung%20Barat%2C%20Jawa%20Barat!5e0!3m2!1sid!2sid!4v1747322272935!5m2!1sid!2sid"
+                            <iframe                             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31678.8802059115!2d107.19736885912606!3d-7.025734550454537!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4z10!3m3!1m2!1s0x2e685eb54a60622d%3A0xbaa410fbc2298484!2sSindangjaya%2C%20Kec.%20Gununghalu%2C%20Kabupaten%20Bandung%20Barat%2C%20Jawa%20Barat!5e0!3m2!1sid!2sid!4v1747322272935!5m2!1sid!2sid"
                                 width="100%"
                                 height="100%"
                                 style={{ border: 0 }}
