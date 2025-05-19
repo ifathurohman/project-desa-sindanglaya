6b23bcb685b6aaa6213e8d3737a3b12339f715c8
ifathurohman <faturahman.ilham@gmail.com>
2025-05-19 17:57:27 +0700

    Update village data with real content

--- src/data/galleryData.ts
+++ src/data/galleryData.ts
@@ -10,74 +10,42 @@ export interface GalleryItem {
 export const galleryData: GalleryItem[] = [
   {
     id: 1,
-    title: 'Pantai Biru Kersik',
-    description: 'Keindahan pantai dengan hamparan pasir putih dan air laut yang jernih.',
-    image: 'https://images.pexels.com/photos/1268082/pexels-photo-1268082.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-    category: 'tourism',
+    title: 'Musyawarah Desa RKPDes 2024',
+    description: 'Kegiatan musyawarah desa untuk menetapkan Rencana Kerja Pemerintah Desa tahun 2024.',
+    image: 'https://images.pexels.com/photos/3184291/pexels-photo-3184291.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    category: 'event',
     date: '15 April 2024'
   },
   {
     id: 2,
-    title: 'Festival Budaya Tahunan',
-    description: 'Penampilan tari tradisional oleh anak-anak desa dalam Festival Budaya Tahunan.',
-    image: 'https://images.pexels.com/photos/7725952/pexels-photo-7725952.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-    category: 'culture',
-    date: '30 Maret 2024'
+    title: 'Pelatihan Pengolahan Kopi',
+    description: 'Petani desa mengikuti pelatihan pengolahan kopi untuk meningkatkan kualitas produksi.',
+    image: 'https://images.pexels.com/photos/1695052/pexels-photo-1695052.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    category: 'development',
+    date: '10 April 2024'
   },
   {
     id: 3,
-    title: 'Peresmian Jalan Desa',
-    description: 'Warga desa dan aparat pemerintah dalam acara peresmian jalan desa yang baru diaspal.',
-    image: 'https://images.pexels.com/photos/5792815/pexels-photo-5792815.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    title: 'Pembangunan Saluran Irigasi',
+    description: 'Proyek pembangunan saluran irigasi di Dusun Cisitu untuk mendukung pertanian.',
+    image: 'https://images.pexels.com/photos/1483880/pexels-photo-1483880.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
     category: 'development',
     date: '5 April 2024'
   },
   {
     id: 4,
-    title: 'Pelatihan Kerajinan Tangan',
-    description: 'Ibu-ibu PKK sedang mengikuti pelatihan pembuatan kerajinan tangan dari bahan daur ulang.',
-    image: 'https://images.pexels.com/photos/950241/pexels-photo-950241.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-    category: 'event',
-    date: '10 April 2024'
+    title: 'Kebun Kopi Desa',
+    description: 'Perkebunan kopi yang menjadi salah satu komoditas unggulan Desa Sindangjaya.',
+    image: 'https://images.pexels.com/photos/2909083/pexels-photo-2909083.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    category: 'agriculture',
+    date: '1 April 2024'
   },
   {
     id: 5,
-    title: 'Gotong Royong Membersihkan Sungai',
-    description: 'Warga desa bergotong royong membersihkan sungai yang menjadi sumber air untuk pertanian dan perikanan.',
-    image: 'https://images.pexels.com/photos/8851096/pexels-photo-8851096.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-    category: 'event',
-    date: '12 Maret 2024'
-  },
-  {
-    id: 6,
-    title: 'Pelestarian Hutan Mangrove',
-    description: 'Kegiatan penanaman pohon mangrove di pesisir pantai untuk mencegah abrasi dan melestarikan ekosistem.',
-    image: 'https://images.pexels.com/photos/2827096/pexels-photo-2827096.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-    category: 'development',
-    date: '8 Maret 2024'
-  },
-  {
-    id: 7,
-    title: 'Panen Raya Padi',
-    description: 'Petani desa sedang melakukan panen raya padi dengan hasil yang melimpah.',
-    image: 'https://images.pexels.com/photos/4947396/pexels-photo-4947396.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-    category: 'event',
-    date: '2 Maret 2024'
-  },
-  {
-    id: 8,
-    title: 'Pertemuan Rutin Warga',
-    description: 'Kegiatan pertemuan rutin warga untuk membahas program dan pembangunan desa.',
-    image: 'https://images.pexels.com/photos/7356370/pexels-photo-7356370.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-    category: 'event',
-    date: '25 Februari 2024'
-  },
-  {
-    id: 9,
-    title: 'Wisata Air Terjun Kersik',
-    description: 'Keindahan air terjun di perbukitan Desa Kersik yang menjadi tujuan wisata alam.',
-    image: 'https://images.pexels.com/photos/1659438/pexels-photo-1659438.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-    category: 'tourism',
-    date: '18 Februari 2024'
+    title: 'Sawah Desa Sindangjaya',
+    description: 'Hamparan sawah yang menjadi sumber penghasilan utama warga desa.',
+    image: 'https://images.pexels.com/photos/2258536/pexels-photo-2258536.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    category: 'agriculture',
+    date: '28 Maret 2024'
   }
 ];
